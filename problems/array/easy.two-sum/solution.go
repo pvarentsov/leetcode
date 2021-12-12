bd4db40bedab5a8b90e7013367efdaf3ed0f12cf
pvarentsov <varentsovpavel@gmail.com>
2021-12-12 12:02:29 +0300

    Code refactoring

--- problems/array/easy.two-sum/solution.go
+++ problems/array/easy.two-sum/solution.go
@@ -4,16 +4,16 @@ package main
 // Status: Passed
 
 func twoSum(nums []int, target int) []int {
-	var out []int
+	var res []int
 
 	for i := 0; i < len(nums); i++ {
 		for j := 0; j < len(nums) && j != i; j++ {
 			if nums[i]+nums[j] == target {
-				out = []int{i, j}
+				res = []int{i, j}
 				break
 			}
 		}
 	}
 
-	return out
+	return res
 }
